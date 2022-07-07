9d269fd939ac48bce87bc47f865f4392e78a0b54
duvangers <94274519+duvangers@users.noreply.github.com>
2022-07-07 16:37:35 -0500

    1

--- api/src/models/ordens.js
+++ api/src/models/ordens.js
@@ -2,6 +2,10 @@ const { Datatypes } = require('sequelize');
 
 const Ordens = (sequelize) => {
     const model = sequelize.define('ordens', {
+        id_user:{
+            type:Datatypes.STRING,
+            allowNull: false,
+        },
         status:{
             type: Datatypes.STRING,
             allowNull: false,
