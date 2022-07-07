a3596c2e2d1f867c8a314996e90f29de25dc0a2d
duvangers <94274519+duvangers@users.noreply.github.com>
2022-07-07 15:48:16 -0500

    AddModelOrders

--- api/src/models/ordens.js
+++ api/src/models/ordens.js
@@ -1,1 +1,15 @@
-const { Datatypes } =   
+const { Datatypes } = require('sequelize');
+
+const Ordens = (sequelize) => {
+    const model = sequelize.define('ordens', {
+        status:{
+            type: Datatypes.STRING,
+            allowNull: false,
+        }
+    })
+
+    return model
+}
+
+module.exports = Ordens
+
