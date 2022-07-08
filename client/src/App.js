1695b4db227150be492b4a20d0d7a81c1c26bcdc
ThonyIucI <anthonyau011798@gmail.com>
2022-07-08 01:49:06 -0500

    Probando Cards container y Card

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./component/home/home";
-import Card from "./component/Card/card";
+import Card from "./component/card/card";
 import Navbar from "./component/navbar/navbar";
+import Cards from "./component/cards/Cards";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/card" element={<Card />} />
+        <Route exact path="/cards" element={<Cards />} />
         <Route exact path="/navbar" element={<Navbar />} />
       </Routes>
     </div>
