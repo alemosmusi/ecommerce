464f0e925cc8dac9aa6fcb448d5f1756fc2ea703
Dv-del <alexis1614@live.com.ar>
2022-07-08 15:02:58 -0300

    main

--- api/src/app.js
+++ api/src/app.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const logger = require("morgan");
+const express = require("express")
+const logger = require("morgan")
 
-const routes = require("./routes");
+const routes = require("./routes")
 
-const app = express();
+const app = express()
 
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.json({ limit: "50mb" }));
-app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }))
+app.use(express.json({ limit: "50mb" }))
+app.use(logger("dev"))
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-app.use("/", routes);
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 
-module.exports = app;
+  next()
+})
+
+app.use("/", routes)
+
+module.exports = app
