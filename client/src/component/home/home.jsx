4bae86f84bbb8466c6eca3124f2f5841c81b8e6f
alemosmusi <alemosmusi@gmail.com>
2022-07-08 01:05:12 -0300

    se creo el wrapper para el home

--- client/src/component/home/home.jsx
+++ client/src/component/home/home.jsx
@@ -1,9 +1,13 @@
+import { Wrapper } from "../wrapper/wrapper";
+
+
 export function Home(){
     
     return (
         
         <div className="home">
             este es el home         
+            <Wrapper></Wrapper>
         
         </div>
     )
