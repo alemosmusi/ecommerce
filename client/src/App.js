57ecd94196b153e6df4b2d0a27bec2c1361a6921
ThonyIucI <anthonyau011798@gmail.com>
2022-07-07 23:21:52 -0500

    ruta de navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./component/home/home";
-import Card from "./component/Card/Card";
+import Card from "./component/Card/card";
+import Navbar from "./component/navbar/navbar";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/card" element={<Card />} />
+        <Route exact path="/navbar" element={<Navbar />} />
       </Routes>
     </div>
   );
