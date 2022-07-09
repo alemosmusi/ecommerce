fd340fa545bd77c55756b668def034748ee35a36
ThonyIucI <anthonyau011798@gmail.com>
2022-07-09 05:59:37 -0500

    Se añadio FindOrCreate al modelo de Brands

--- api/src/models/brands.js
+++ api/src/models/brands.js
@@ -1,31 +1,36 @@
-const { DataTypes } = require('sequelize')
+const { DataTypes } = require("sequelize");
 
 const Brands = (sequelize) => {
-    const model = sequelize.define('brands', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        }
-    },  {
-            createdAt: false,
-            updatedAt: false
-        }
-    )
-
-    const preStart = () => {
-        const response = require('../temporal-json/brands.json')
-
-        response.forEach(async (value) => {
-            await model.create({
-                name: value.name
-            })
-        })
+  const model = sequelize.define(
+    "brands",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      createdAt: false,
+      updatedAt: false,
     }
+  );
 
-    setTimeout(preStart, 3000)
+  const preStart = () => {
+    const response = require("../temporal-json/brands.json");
 
-    return model
-}
+    response.forEach(async (value) => {
+      await model.findOrCreate({
+        where: {
+          name: value.name,
+        },
+      });
+    });
+  };
 
-module.exports = Brands
+  setTimeout(preStart, 3000);
+
+  return model;
+};
+
+module.exports = Brands;
