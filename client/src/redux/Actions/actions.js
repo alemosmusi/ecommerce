980e3a0beb9cd8dfe1ed12e245f4591e1ef448c5
Dv-del <alexis1614@live.com.ar>
2022-07-08 21:38:44 -0300

    redux

--- client/src/redux/Actions/actions.js
+++ client/src/redux/Actions/actions.js
@@ -1,31 +1,53 @@
 import axios from "axios";
-import { URL_ALL_CATEGORIES, URL_ALL_SHOES } from "../URL/URL";
+
 export const GET_ALL_SHOES = "GET_ALL_SHOES";
+export const GET_ALL_BRANDS = "GET_ALL_BRANDS"
 export const GET_ALL_CATEGORIES = "GET_ALL_CATEGORIES";
+export const GET_ALL_COLORS = "GET_ALL_COLORS";
+export const GET_ALL_GENDERS = "GET_ALL_GENDERS";
+
+export const GET_FILTERS_BRANDS = "GET_FILTERS_BRANDS"
+export const GET_FILTERS_CATEGORY = "GET_FILTERS_CATEGORY"
+export const GET_FILTERS_COLORS = "GET_FILTERS_COLORS"
+export const GET_FILTERS_GENDERS = "GET_FILTERS_GENDERS"
+export const GET_FILTERS_SIZE_RANGE = "GET_FILTERS_SIZE_RANGE"
+
+const URL = "http://localhost:3001"
 
 export const getAllShoes = () => {
   return async function (dispatch) {
     try {
-      let info = await axios(URL_ALL_SHOES);
+      let info = await axios(`${URL}/shoes`);
       return dispatch({
         type: GET_ALL_SHOES,
         payload: info.data,
       });
     } catch (error) {
       console.log(error.response);
-      //   alert(
-      //     error.response && error.response.data
-      //       ? error.response.data
-      //       : error.message
-      //   );
     }
   };
 };
 
+export const getAllBrands = () => {
+  return async function (dispatch) {
+    try {
+      const response = await axios(`${URL}/brands`);
+      const { data: payload } = response
+
+      return dispatch({
+        type: GET_ALL_BRANDS,
+        payload
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
 export const getAllCategories = () => {
   return async function (dispatch) {
     try {
-      let categories = await axios(URL_ALL_CATEGORIES);
+      let categories = await axios(`${URL}/categories`);
       return dispatch({
         type: GET_ALL_CATEGORIES,
         payload: categories.data,
@@ -33,5 +55,73 @@ export const getAllCategories = () => {
     } catch (error) {
       console.log(error.response);
     }
-  };
+  }
 };
+
+export const getAllColors = () => {
+  return async function (dispatch) {
+    try {
+      const response = await axios(`${URL}/colors`);
+      const { data: payload } = response
+
+      return dispatch({
+        type: GET_ALL_COLORS,
+        payload
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export const getAllGenders = () => {
+  return async function (dispatch) {
+    try {
+      const response = await axios(`${URL}/genders`);
+      const { data: payload } = response
+
+      return dispatch({
+        type: GET_ALL_GENDERS,
+        payload
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export const getFilterBrands = (payload) => {
+  return async function (dispatch) {
+    return dispatch({
+      type: GET_FILTERS_BRANDS,
+      payload
+    })
+  }
+}
+
+export const getFilterCategories = (payload) => {
+  return async function (dispatch) {
+    return dispatch({
+      type: GET_FILTERS_CATEGORY,
+      payload
+    })
+  }
+}
+
+export const getFilterColors = (payload) => {
+  return async function (dispatch) {
+    return dispatch({
+      type: GET_FILTERS_COLORS,
+      payload
+    })
+  }
+}
+
+export const getFilterGenders = (payload) => {
+  return async function (dispatch) {
+    return dispatch({
+      type: GET_FILTERS_GENDERS,
+      payload
+    })
+  }
+}
--- client/src/redux/Reducer/reducer.js
+++ client/src/redux/Reducer/reducer.js
@@ -1,25 +1,54 @@
-import { GET_ALL_SHOES ,  GET_ALL_CATEGORIES} from "../Actions/actions";
+import * as actionTypes from "../Actions/actions";
 
 const initialState = {
-Shoes:[],
-categories:[]
-  };
-  
-  const rootReducer = (state = initialState, action) => {
+  Shoes : [],
+  Filters : [],
+  Categories : [],
+  Brands : [],
+  Colors : [],
+  Genders: []
+};
 
-    switch (action.type){
-      case GET_ALL_SHOES:
-          return{
-            ...state,
-            Shoes:action.payload,
-          }
-      case  GET_ALL_CATEGORIES:
-            return{
-              ...state,
-              categories:action.payload,
-            }
-          default : return state
-        } 
+const rootReducer = (state = initialState, action) => {
+  switch (action.type){
+    case actionTypes.GET_ALL_SHOES:
+      return  {
+        ...state,
+        Shoes: action.payload,
+        Filters: action.payload
+      }
+    case actionTypes.GET_ALL_BRANDS: 
+      return {
+        ...state,
+        Brands: action.payload
+      }
+    case actionTypes.GET_ALL_CATEGORIES:
+      return  {
+        ...state,
+        Categories: action.payload,
+      }
+    case actionTypes.GET_ALL_COLORS: 
+      return {
+        ...state,
+        Colors: action.payload
+      }
+    case actionTypes.GET_ALL_GENDERS:
+      return {
+        ...state,
+        Genders: action.payload
+      }
+    case actionTypes.GET_FILTERS_BRANDS:
+      const Filters = Filters.filter(value => {
+        return value.brand_name === action.payload
+      })
+
+      return {
+        ...state,
+        Filters
+      }
+    default : 
+      return state
+  } 
 }
 
 export default rootReducer;
