7945662b95503e7b653c9b37680c4cffa5c4d0d5
ThonyIucI <anthonyau011798@gmail.com>
2022-07-12 15:19:10 -0500

    com oaneladmin

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./component/home/home";
@@ -8,10 +9,10 @@ import { Questions } from "./component/question/question";
 import Copyright from "./component/copyright/copyright";
 
 import Terms from "./component/terms/terms";
-import "./App.css";
 import CarShopContainer from "./component/carShop/Container/CarshopContainer";
 import GeneralContainer from "./component/cardFilters/GeneralContainer";
 import { InfoProfile } from "./component/infoProfile/infoProfile.jsx";
+import ListProducts from "./panelAdmin/listProducts/ListProducts";
 
 function App() {
   return (
@@ -19,13 +20,26 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/getDetailsProduct/:id" element={<ProductDetail />} />
+        <Route
+          exact
+          path="/getDetailsProduct/:id"
+          element={<ProductDetail />}
+        />
         <Route exact path="/questions" element={<Questions />} />
         <Route exact path="/copyright" element={<Copyright />} />
         <Route exact path="/terms" element={<Terms />} />
         <Route exact path="/filters" element={<GeneralContainer />} />
         <Route exact path="/carshop" element={<CarShopContainer />} />
         <Route exact path="/infoperfil" element={<InfoProfile />} />
+        <Route path="admin">
+          <Route index element={<ListProducts />} />
+          <Route path="products" element={<ListProducts />} />
+          <Route
+            path="new"
+            // element={<New inputs={userInputs} title="Add New User" />}
+          />
+        </Route>
+        ;
       </Routes>
       <Footer />
     </div>
