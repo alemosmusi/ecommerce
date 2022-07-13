ac2f2b16715090c79c4acf050d362c35686543ce
duvangers <94274519+duvangers@users.noreply.github.com>
2022-07-13 14:09:03 -0500

    adddd

--- client/src/panelUser/components/dataTable/Datatable2.jsx
+++ client/src/panelUser/components/dataTable/Datatable2.jsx
@@ -20,112 +20,113 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 // import { getAllShoes } from "../../../redux/actions";
 // // const ordersColumns
 
-function createData(name, calories, fat, carbs, protein, price) {
-    
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      {
-        id: 1,
-        nickname: "Shadow",
-        img: "https://image.goat.com/attachments/product_template_pictures/images/011/119/994/original/218099_00.png.png",
-        color: 'Black',
-        price: 16000,
-        talla: 10.5,
-      },
-      {
-        id: 2,
-        nickname: "Bred",
-        img: "https://image.goat.com/attachments/product_template_pictures/images/020/806/444/original/507844_00.png.png",
-        color: 'Black',
-        price: 14000,
-        talla: 10,
-      },
-    ],
-  };
-}
-const rows = [
-  createData('1524', 4, 19000, 24, 4.0, 3.99),
-  createData('2656', 2, 20500, 37, 4.3, 4.99),
-  createData('3544', 1, 7000, 24, 6.0, 3.79),
-  createData('4456', 1, 8000, 67, 4.3, 2.5),
-  createData('5544', 3, 19000, 49, 3.9, 1.5),
-];
-
-function Row(props) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
-
-  return (
-    <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.calories * row.fat}</TableCell>
-        {/* <TableCell align="right">{row.carbs}</TableCell> */}
-        {/* <TableCell align="right">{row.protein}</TableCell> */}
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Productos</TableCell>
-                    <TableCell>Color</TableCell>
-                    <TableCell>Talla</TableCell>
-                    <TableCell>Unidades</TableCell>
-                    <TableCell align="right">Price</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.id}>
-                      <TableCell component="th" scope="row">
-                      <img className="cellImg" src={historyRow.img} alt="avatar" />
-                      </TableCell>
-                      <TableCell>{historyRow.color}</TableCell>
-                      <TableCell>{historyRow.talla}</TableCell>
-                      <TableCell>{historyRow.id}</TableCell>
-                      <TableCell align="right">{historyRow.price}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.price * historyRow.id * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
-}
 
 
 export default function CollapsibleTable() {
+  function createData(name, calories, fat, carbs, protein, price) {
+    
+    return {
+      name,
+      calories,
+      fat,
+      carbs,
+      protein,
+      price,
+      history: [
+        {
+          id: 1,
+          nickname: "Shadow",
+          img: "https://image.goat.com/attachments/product_template_pictures/images/011/119/994/original/218099_00.png.png",
+          color: 'Black',
+          price: 16000,
+          talla: 10.5,
+        },
+        {
+          id: 2,
+          nickname: "Bred",
+          img: "https://image.goat.com/attachments/product_template_pictures/images/020/806/444/original/507844_00.png.png",
+          color: 'Black',
+          price: 14000,
+          talla: 10,
+        },
+      ],
+    };
+  }
+  const rows = [
+    createData('1524', 4, 19000, 24, 4.0, "24/01/2022"),
+    createData('2656', 2, 20500, 37, 4.3, "27/09/2022"),
+    createData('3544', 1, 7000, 24, 6.0, "24/09/2022"),
+    createData('4456', 1, 8000, 67, 4.3, "24/10/2022"),
+    createData('5544', 3, 19000, 49, 3.9, "24/11/2022"),
+  ];
+  
+  function Row(props) {
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
+  
+    return (
+      <React.Fragment>
+        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+          <TableCell>
+            <IconButton
+              aria-label="expand row"
+              size="small"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </TableCell>
+          <TableCell component="th" scope="row">
+            {row.name}
+          </TableCell>
+          <TableCell align="right">{row.calories}</TableCell>
+          <TableCell align="right">{row.calories * row.fat}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box sx={{ margin: 1 }}>
+                <Typography variant="h6" gutterBottom component="div">
+                  History
+                </Typography>
+                <Table size="small" aria-label="purchases">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Date Order</TableCell>
+                      <TableCell>Productos</TableCell>
+                      <TableCell>Color</TableCell>
+                      <TableCell>Talla</TableCell>
+                      <TableCell>Unidades</TableCell>
+                      <TableCell align="right">Price</TableCell>
+                      <TableCell align="right">Total price ($)</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {row.history.map((historyRow) => (
+                      <TableRow key={historyRow.id}>
+                        <TableCell>{row.price}</TableCell>
+                        <TableCell component="th" scope="row">
+                        <img className="cellImg" src={historyRow.img} alt="avatar" />
+                        </TableCell>
+                        <TableCell>{historyRow.color}</TableCell>
+                        <TableCell>{historyRow.talla}</TableCell>
+                        <TableCell>{historyRow.id}</TableCell>
+                        <TableCell align="right">{historyRow.price}</TableCell>
+                        <TableCell align="right">
+                          {Math.round(historyRow.price * historyRow.id * 100) / 100}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    );
+  }
+  
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
@@ -134,8 +135,6 @@ export default function CollapsibleTable() {
             <TableCell />
             <TableCell>Ordenes (Id)</TableCell>
             <TableCell align="right">Product (#)</TableCell>
-            {/* <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell> */}
             <TableCell align="right">Total de Orden($)</TableCell>
           </TableRow>
         </TableHead>
@@ -148,8 +147,3 @@ export default function CollapsibleTable() {
     </TableContainer>
   );
 }
-
-// export default Datatable;
-// module.export ={
-//     CollapsibleTable
-// };
