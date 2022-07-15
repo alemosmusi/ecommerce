800449f3ebb1e74f128d2684aa41b185b509001a
Dv-del <alexis1614@live.com.ar>
2022-07-14 21:30:26 -0300

    main

--- api/src/controllers/ordens.js
+++ api/src/controllers/ordens.js
@@ -1,8 +1,10 @@
-const { modelOrdens } = require("../db")
+const { modelOrdens, modelUsers, modelProducts } = require("../db")
 
 const getOrdens = async (req, res) => {
     try {
-        const response = await modelOrdens.findAll({raw: true})
+        const response = await modelOrdens.findAll({
+            include: [modelUsers, modelProducts]
+        },{raw: true})
         res.status(200).json(response)
     } catch (error) {
         res.status(500).send({msg: 'Error internal server', error})
@@ -10,46 +12,56 @@ const getOrdens = async (req, res) => {
 }
 
 const createOrden = async (req, res) => {
-    const { products, price_total, amount } = req.body
+    const { id } = req.params
+    const { products, details, price_total, amount_total } = req.body
 
     try {
-        const orden = await modelOrdens.create({
-            amount: products.reduce((a, value) => a += amount, 0),
-            //price_total: products.
-        })
-    } catch (error) {
+        const user = await modelUsers.findByPk(id)
+        if (!user) return res.status(200).send({msg: 'User not exists database!'})
 
+        const order = await modelOrdens.create({
+            amount_total,
+            price_total,
+            details
+        })
+
+        order.setUser(id)
+        order.setProducts(products)
+        
+        res.status(200).send({msg: 'Order created successfully!'})
+    } catch (error) {
+        res.status(500).send({msg: 'Error internal server', error})
     }
 }
 
-/*products: [
-    {
-        productID: 1,
-        size: 10,
-        amount: 3,
-        price: 10
-    },
-    {
-        productID: 1,
-        size: 10,
-        amount: 3
-    }
-    {
-        productID: 1,
-        size: 10,
-        amount: 3
-    }
-    {
-        productID: 1,
-        size: 10,
-        amount: 3
-    }
-]*/
-
 const updateOrden = () => {}
+
+const getOrdensUser = async (req, res   ) => {
+    const { id } = req.params
+
+    try {
+        const user = await modelUsers.findByPk(id)
+        if (!user) return res.status(200).send({msg: 'User not exists database!'})
+
+        const response = await modelUsers.findOne({
+            where: {
+                id
+            },
+            include: {
+                model: modelOrdens,
+                include: modelProducts
+            }
+        })
+
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).send({msg: 'Error internal server', error})
+    }
+}
 
 module.exports = {
     getOrdens,
     createOrden,
-    updateOrden
+    updateOrden,
+    getOrdensUser
 }
--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -2,7 +2,7 @@ const { modelUsers } = require("../db")
 
 const getUsers = async (req, res) => {
     try {
-        const response = await modelUsers.findAll()
+        const response = await modelUsers.findAll({raw: true})
         res.status(200).json(response)
     } catch (error) {
         res.status(500).send({msg: 'Error internal server'})
